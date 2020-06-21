d754dbde2be9a083296b0fc3a237ec0d92ec618e
Muhammad Rashid <55427374+MdRashid62@users.noreply.github.com>
2020-06-21 16:10:36 +0500

    git commit

--- src/Layout/Footer.js
+++ src/Layout/Footer.js
@@ -4,10 +4,9 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className="footer">
- 
-    <p>Powered by <a href="https://cosmicjs.com">CosmicJs</a> created by <a href="https://github.com/3ndrius">3ndrius</a> Inspired by <a href="https://medium.com/">Medium</a> </p>
-    
-     
+
+    <p>Powered by <a href="https://cosmicjs.com">CosmicJs</a> Inspired by <a href="https://medium.com/">Medium</a> <a href="https://github.com/MdRashid62" title="Visit on GitHub"> | Muhammad Rashid</a> </p>
+
     </div>
   )
 }
